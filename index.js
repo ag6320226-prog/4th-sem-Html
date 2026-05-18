3684b782af6154c583e2d06149be166f1a6a58bf
Ayush Gupta <ag6320226@gmail.com>
2026-05-18 15:00:44 +0530

    lect7

--- index.js
+++ index.js
@@ -1,173 +1,89 @@
-// let input = document.querySelector('input');
-// let btn = document.querySelector('button');
-// let list = document.getElementById('list');
-// btn.addEventListener("click",()=>{
-
-//     let city=input.value
-//     let apik='9f6290d6cda9a36a63755fadee71f83d'
-//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9f6290d6cda9a36a63755fadee71f83d`).
-
-//         return data.json()
-// }).then ((info)=>{
-//     console.log(val);
-//     let temp=val.main.temp;
-//     let tempC=temp-273.15;
-//     console.log(tempC);
-// })
-
-//     fetch(`https://api.tvmaze.com/search/shows?q=${data}`).
-//     then((info)=>{
-//         return info.json()
-
-//     }).then((val)=>{
-//        console.log(val);
-
-//             show(val)
-//     })
-
-// })
+// const arr =[
+//     'https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/14235/production/_100058428_mediaitem100058424.jpg',
+//     'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+//     'https://img.magnific.com/free-photo/closeup-shot-beautiful-butterfly-with-interesting-textures-orange-petaled-flower_181624-7640.jpg?semt=ais_hybrid&w=740&q=80',
+//     'https://png.pngtree.com/thumb_back/fh260/background/20230411/pngtree-nature-forest-sun-ecology-image_2256183.jpg',
+   
+// ]
+// const imageEl = document.querySelector('img');
+// let i=0;
+// setInterval(function(){
+//     imageEl.setAttribute('src',arr[i]);
+//     i=(i+1)%arr.length;
+// },2500)
 
 
-// function show(val){
-
-
-//   for(let i of val){
-//     let img=  document.createElement("img")
-//     console.log(i.show.image.original,"heheh");
-//     img.setAttribute("src",i.show.image.original)
-    
-    
-//     img.setAttribute("src",i.show.image.original)   
-//     list.append(img)
-
-//   }
-// //   img.setAttribute("src",val[0].show.image.
-// //     original)
-
-//     // list.appendChild(img)
-
+// function step1(fn){
+//     setTimeout(()=>{
+//         console.log("select3eedd");
+//         fn();
+//     },5000)
 // }
-
-
-
-
-
-// // function fetchData(searchText) {
-// //   axios.get(`https://api.tvmaze.com/search/shows?q=${searchText}`)
-// //   .then(function(response) {
-// //     manipulateDOM(response.data);
-// // })
-// // }
-
-// // function manipulateDOM(data) {
-// //   while(list.firstChild) {
-// //     list.firstChild.remove();
-// //   }
-
-// //   for (let d of data) {
-// //     if (d.show.image) {
-// //       let img = document.createElement('img');
-// //       img.setAttribute('src', d.show.image.original);
-// //       list.appendChild(img);
-// //     }  
-// //   }
-// // }
-
-// // btn.addEventListener('click', function() {
-// //   let searchText = input.value;
-// //   let data = fetchData(searchText);
-// // })
-// // })
-
-
-
-
-// // let apiK='9f6290d6cda9a36a63755fadee71f83d'
-// // let city="bhopal"
-// // `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiK}`
-// let input = document.querySelector('input');
-// let btn = document.querySelector('button');
-// let list = document.getElementById('list');
-// btn.addEventListener("click",()=>{
-//     let data=input.value
-
-//     fetch(`https://api.tvmaze.com/search/shows?q=${data}`).
-//     then((info)=>{
-//         return info.json()
-
-//     }).then((val)=>{
-//        console.log(val);
-
-//             show(val)
-//     })
-
-// })
-
-
-// function show(val){
-
-
-//   for(let i of val){
-//     let img=  document.createElement("img")
-//     console.log(i.show.image.original,"heheh");
-//     img.setAttribute("src",i.show.image.original)
-    
-    
-//     img.setAttribute("src",i.show.image.original)   
-//     list.append(img)
-
-//   }
-// //   img.setAttribute("src",val[0].show.image.
-// //     original)
-
-//     // list.appendChild(img)
-
+// function step2(fn){
+//     setTimeout(()=>{
+//         console.log("filtered");
+//         fn();
+//     },4000)
 // }
-
-
-
-
-
-// // function fetchData(searchText) {
-// //   axios.get(`https://api.tvmaze.com/search/shows?q=${searchText}`)
-// //   .then(function(response) {
-// //     manipulateDOM(response.data);
-// // })
-// // }
-
-// // function manipulateDOM(data) {
-// //   while(list.firstChild) {
-// //     list.firstChild.remove();
-// //   }
-
-// //   for (let d of data) {
-// //     if (d.show.image) {
-// //       let img = document.createElement('img');
-// //       img.setAttribute('src', d.show.image.original);
-// //       list.appendChild(img);
-// //     }  
-// //   }
-// // }
-
-// // btn.addEventListener('click', function() {
-// //   let searchText = input.value;
-// //   let data = fetchData(searchText);
-// // })
-// // })
-
-
-
-
-// let apiK='9f6290d6cda9a36a63755fadee71f83d'
-// let city="bhopal"
-// `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiK}`
-// .then((res)=>{
-//     return res.json()
-// }).then((val)=>{
-//     console.log(val);
-//     let temp=val.main.temp;
-//     let tempC=temp-273.15;
-//     console.log(tempC);
-    
-
+// function step3(fn){
+//     setTimeout(()=>{
+//         console.log("captioned");
+//         fn();
+//     },3000)
+// }
+// function step4(fn){
+//     setTimeout(()=>{
+//         console.log("uploaded");
+//         fn();
+//     },2000)
+// }
+// step1( function(){
+// step2(function(){
+//     step3(function(){
+//         step4()
+//     })
 // })
+// })
+
+// let p= new Promise((res,rej)=>{
+//     res()
+//     // rej()
+// })
+// p.then(()=>{
+//     console.log("6 how are you");
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+// function step1(){
+//     return new Promise((res,rej)=>{
+//         setTimeout(()=>{
+//             console.log("select3eedd");
+//             res();
+//         },5000)
+//     })
+// }
+// function step2(){
+//     return new Promise((res ,rej)=>{
+//         setTimeout(()=>{
+//             console.log("filtered");
+//             res();
+
+//         },4000)
+//     })
+// }
+// async function processImage(){
+//     await step1();
+//     await step2();
+//     console.log("done");
+// }
+// call()
+fetch("https://jsonplaceholder.typicode.com/todos").
+then((data)=>{
+return data.json()
+}).then((val)=>{
+    console.log(val);
+}).catch((err)=>{
+    console.log(err);
+})
+
